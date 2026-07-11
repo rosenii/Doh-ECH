9a7e6c5a6f5483b643461dbba51c3cfff2aac5a6
rosenii <2273657807mm@gmail.com>
2026-07-11 12:25:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,14 +18,14 @@
 // ===================== 全局配置 =====================
 const UPSTREAM_DNS_GOOGLE = 'https://dns.google/dns-query';
 const UPSTREAM_JSON_GOOGLE = 'https://dns.google/resolve';
-const UPSTREAM_DNS_CUSTOM = 'https://dns.google/dns-query';
-const UPSTREAM_JSON_CUSTOM = 'https://dns.google/resolve';
+const UPSTREAM_DNS_CUSTOM = 'https://dns11.quad9.net/dns-query';
+const UPSTREAM_JSON_CUSTOM = 'https://dns11.quad9.net/dns-query';
 const IPV4_ONLY_DOMAINS = ["twitter.com", "x.com", "t.co", "twimg.com"];
 const BUILTIN_HINTS = [
     {
         domains: ["*.google.com", "*.googleapis.com","*.google.com.hk", "*.googleusercontent.com","*.gstatic.com","*.youtube.com","*.ytimg.com","*.ggpht.com"],
         ips: ["2001:4860:4827:7700:abcd:ef01:2345:6789","2001:4860:4827:7700:1357:2468:369a:48bf","2001:4860:4827:7700:f001:f002:f003:f004","2001:4860:4827:7700:0000:abcd:1234:5679","2001:4860:4827:7700:1411:1222:3343:4144","2001:4860:4827:7700:64:22:33:44"],
-       // noA: true,
+        noA: true,
         noAAAA: false
     },
     {
